13f94e1f1b1477858958e37822383f7c10f6e21f
Florent Pruvost <florent.pruvost@inria.fr>
2022-01-20 16:19:28 +0100

    Read the solutions in the right order to make the video.

--- heat_plot.py
+++ heat_plot.py
@@ -1,9 +1,10 @@
-import os 
+import os
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 sols= np.array([ np.loadtxt(f) for f in os.listdir('.') if 'sol' in f])
 for i in range(1,sols.shape[0]):
-  plt.imsave('%05d.png' % i, sols[i,:,:],cmap = plt.cm.jet,vmin=0,vmax=1)
+  sol = np.loadtxt('sol_%05d' % (i-1))
+  plt.imsave('%05d.png' % (i-1), sol,cmap = plt.cm.jet,vmin=0,vmax=1)
 comm="ffmpeg -i %05d.png heat.avi"
 os.system(comm)
