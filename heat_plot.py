ba9950e689fc83ea64ef4f00cfa7dbd8d47b00ac
Florent Pruvost <florent.pruvost@inria.fr>
2023-11-14 16:10:27 +0100

    Improve heat_plot script to be tested

--- heat_plot.py
+++ heat_plot.py
@@ -2,9 +2,26 @@ import os
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-sols= np.array([ np.loadtxt(f) for f in os.listdir('.') if 'sol' in f])
-for i in range(1,sols.shape[0]):
-  sol = np.loadtxt('sol_%05d' % (i-1))
-  plt.imsave('%05d.png' % (i-1), sol,cmap = plt.cm.jet,vmin=0,vmax=1)
-comm="ffmpeg -i %05d.png heat.avi"
-os.system(comm)
+
+def heat_plot():
+
+    #on compte le nombre de fichier (et on peut s'apercevoir qu'il ne sont pas lu dans l'ordre alphabétique)
+    nb = 0
+    for f in os.listdir('.'):
+        if 'sol' in f:
+            nb = nb + 1
+
+    file = []
+    # on construit un tableau avec les noms de fichier dans l'ordre alphabétique
+    for i in range(0,nb):
+        file.append("sol_" + str('{:05d}'.format(i)))
+
+    sols= np.array([ np.loadtxt(f) for f in file], dtype=object)
+    for i in range(1,sols.shape[0]):
+        plt.imsave('%05d.png' % i, sols[i],cmap = plt.cm.jet,vmin=0,vmax=1)
+
+    comm="ffmpeg -i %05d.png heat.avi"
+    os.system(comm)
+
+if __name__ == '__main__':
+    heat_plot()
